cbcdd6d0f8a58c391075ab50cce9ca762a81848b
Coulson Kgathi <ckgathi@gmail.com>
2021-05-18 14:36:26 +0200

    update setup urls and repo name

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
-    name='edc-calender',
+    name='edc-calendar',
     version='0.1.1',
     author=u'Software Engineering & Data Management',
     author_email='se-dmc@bhp.org.bw',
     packages=find_packages(),
     include_package_data=True,
-    url='https://github.com/botswana-harvard/edc-calender',
+    url='https://github.com/botswana-harvard/edc-calendar',
     license='GPL licence, see LICENCE',
     description='EDC calender for events management.',
     long_description=README,
